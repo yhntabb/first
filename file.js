289974be550d8e6043d3311c923f0903273a41f0
yhntabb <56481529+yhntabb@users.noreply.github.com>
2020-07-16 00:16:16 -0400

    file.js

---
+++ file.js
@@ -0,0 +1,48 @@
+const processForm = form => {
+    const data = new FormData(form)
+    data.append('form-name', 'newsletter');
+    fetch('/', {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      form.innerHTML = `<div class="form--success">Almost there! Check your inbox for a confirmation e-mail.</div>`;
+    })
+    .catch(error => {
+      form.innerHTML = `<div class="form--error">Error: ${error}</div>`;
+    })
+  }
+
+  const emailForm = document.querySelector('.email-form')
+if (emailForm) {
+  emailForm.addEventListener('submit', e => {
+    e.preventDefault();
+    processForm(emailForm);
+  })
+}
+
+[build]
+  base = "."
+  functions = "./functions"
+
+
+  require('dotenv').config()
+const fetch = require('node-fetch')
+const { EMAIL_TOKEN } = process.env
+exports.handler = async event => {
+  const email = JSON.parse(event.body).payload.email
+  console.log(`Recieved a submission: ${email}`)
+  return fetch('https://api.buttondown.email/v1/subscribers', {
+    method: 'POST',
+    headers: {
+      Authorization: `Token ${EMAIL_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(`Submitted to Buttondown:\n ${data}`)
+    })
+    .catch(error => ({ statusCode: 422, body: String(error) }))
+}
